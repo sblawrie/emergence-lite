a28cb67846536ed3894019d8bceff41ff157a500
Scot Lawrie <sblawrie@gmail.com>
2013-04-05 13:17:14 -0300

    Added SQL::createFromClass($tableName, $className)
    Generates Raw PHP for a class definition based on a pre-existing table.

--- Library/Classes/SQL.class.php
+++ Library/Classes/SQL.class.php
@@ -152,6 +152,61 @@ class SQL
 		return $createSQL;
 	}
 	
+	static public function getCreateClass($tableName, $className)
+	{
+		$db_name = DB::oneValue('SELECT DATABASE()');
+		$sql = "SELECT `COLUMN_NAME`, `DATA_TYPE`, `COLUMN_KEY`, `EXTRA`
+				FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+				WHERE `TABLE_SCHEMA`='$db_name' 
+				AND `TABLE_NAME`='$tableName';";
+		$Columns = DB::allRecords($sql);
+		$str = '<?php' . "\n\n //Create Class From Table \n \n" . 'class ' . $className . ' extends ActiveRecord
+{
+
+'. "\t" . '// ActiveRecord configuration
+'. "\t" . 'static public $tableName = \'' . $tableName . '\';
+'. "\t" . 'static public $singularNoun = \'' . $className . '\';
+'. "\t" . 'static public $pluralNoun = \'' . $tableName . '\';
+
+'. "\t" . 'static public $rootClass = __CLASS__;
+'. "\t" . 'static public $defaultClass = __CLASS__;
+'. "\t" . 'static public $subClasses = array(__CLASS__);';
+		$fields = array();
+		foreach($Columns as $Column)
+		{
+			$field_array = array();
+			if($Column['COLUMN_KEY']=='PRI' && $Column['COLUMN_NAME']!='ID')
+			{
+				$str .= "\n \n\t" .  'static public $primaryKey' . " = '{$Column['COLUMN_NAME']}';";
+			}
+			
+			$Map = array('varchar'=>'string', 'mediumtext'=>'string', 'text'=>'string', 'largetext'=>'text', 'int'=>'integer', 'date'=>'timestamp', 'datetime'=>'timestamp', 'float'=>'decimal', 'tinyint'=>'integer');
+			$type = (isset($Map[$Column['DATA_TYPE']]))?$Map[$Column['DATA_TYPE']]:$Column['DATA_TYPE'];
+			if($type=='string')
+			{
+				$fields[] = "'{$Column['COLUMN_NAME']}'";
+			}
+			else
+			{
+
+		        $field_array[] = "'type' => '$type'";
+		  
+		        if($Column['EXTRA']=='auto_increment')
+		        {
+			        $field_array[] = "'autoincrement' => true";
+		        }
+		        
+		        $fields[] = "'{$Column['COLUMN_NAME']}' => array(\n\t\t\t" . implode("\n\t\t\t,", $field_array) . "\n\t\t)";
+			}
+			
+		}
+		
+		$str .= "\n\n\t" . 'static public $fields = array(' . "\n\t\t" . implode("\n\t\t,", $fields) . "\n\t" . ");\n}";
+		echo $str;
+		
+	}
+
+	
 	static public function getSQLType($field)
 	{
 		switch($field['type'])
